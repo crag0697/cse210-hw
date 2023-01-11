e84097c0aa0b94d9a3d4bd256d2614cb7535e8a2
crag0697 <craiga.mortensen@gmail.com>
2023-01-11 13:48:49 -0700

    Completed all assignments

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int input = -1;
+        
+
+        List<int> numbers = new List<int>();
+
+        while (input != 0)
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        }
+
+        int total = numbers.Sum();
+        float average = ((float)total) / numbers.Count;
+        int maximum = numbers.Max();
+
+                
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maximum}");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string GetUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int GetFavNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int fav_number = int.Parse(Console.ReadLine());
+            return fav_number;
+        }
+
+        static int SquareNumber(int fav_number)
+        {
+            int square = fav_number * fav_number;
+            return square;
+        }
+
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
+
+        {
+            DisplayMessage();
+
+            string user_name = GetUserName();
+            int user_num = GetFavNumber();
+            int square_num = SquareNumber(user_num);
+
+            DisplayResult(user_name, square_num);
+
+        }
     }
 }
